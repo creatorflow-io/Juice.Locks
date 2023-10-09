764826f040187b8fee819482b7e135adb25ec80b
ductran <duc@hdvietnam.com.vn>
2023-10-09 21:12:20 +0700

    Separate from Juice

---
+++ src/Juice.Locks.Redis/DependencyInjection/LocksServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Juice.Locks.Redis
+{
+    public static class LocksServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRedLock(this IServiceCollection services, Action<RedisOptions> configure)
+        {
+            services.Configure<RedisOptions>(configure);
+            return services.AddSingleton<IDistributedLock, RedLock>();
+        }
+    }
+}
